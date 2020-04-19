bfb53f0e6e52adb726b5dc37b72f5446e9dd0214
Michael <michael.lee.allerby@gmail.com>
2020-04-19 18:14:14 +1200

    production settings for google Oauth

--- config/environment.js
+++ config/environment.js
@@ -78,6 +78,10 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.DS.host = process.env.API_HOST || 'https://gemino-api.herokuapp.com';
+
+    torii.providers['google-oauth2'].tokenEchangeUri = 'https://gemino-api.herokuapp.com/api/authenticate_with_google';
+    torii.providers['google-oauth2'].redirectUri = 'https://gemino-app.herokuapp.com/torii/redirect.html';
+    
     // const clientHost = process.env.CLIENT_HOST || 'https://gemino-app.herokuapp.com';
     // here you can enable a production-specific feature
   }
